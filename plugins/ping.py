50e154fdb0fe94e1647b41390f27f1de25c9231b
UNKNOWN-BOYY <135582477+UNKNOWN-BOYY@users.noreply.github.com>
2025-06-13 22:05:32 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,6 +1,6 @@
-
 from pyrogram import Client, filters
+from pyrogram.types import Message
 
 @Client.on_message(filters.command("ping", prefixes=".") & filters.me)
-async def ping_handler(client, message):
-    await message.reply_text("Pong!")
+async def ping_handler(client: Client, message: Message):
+    await message.reply_text("✅ Pong! Bot is alive.")
