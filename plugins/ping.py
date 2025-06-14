e86165f87a9633b225e10ae264c4d9ed742145f8
UNKNOWN-BOYY <135582477+UNKNOWN-BOYY@users.noreply.github.com>
2025-06-14 08:50:58 +0530

    Delete plugins directory

--- plugins/ping.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-@Client.on_message(filters.command("ping", prefixes=".") & filters.me)
-async def ping_handler(client: Client, message: Message):
-    await message.reply_text("✅ Pong! Bot is alive.")
